fc46626478a40c1c86d52788f4cf70ef17edab1e
aleruizzs <alejandroruizsalazar21@gmail.com>
2026-09-02 09:03:16 +0200

    Nuevo endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -16,5 +16,10 @@ export class AuthController {
     @Body() authCredentialsDto: AuthCredentialsDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
+  }
+  
+  @Get('/alex')
+  getAlexEndpoint(){
+    return "No me crees? Pues flipa";
   }
 }

1078d82400c8b6fbfcfc5f32f4023a6a30c80075
aleruizzs <alejandroruizsalazar21@gmail.com>
2026-09-18 08:00:32 +0200

    updated id format

--- terraform/modules/github_iam/main.tf
+++ terraform/modules/github_iam/main.tf
@@ -15,7 +15,10 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:aleruizzs/nestjs-task-management:*"
+            "token.actions.githubusercontent.com:sub" = [
+              "repo:aleruizzs/nestjs-task-management:*",
+              "repo:aleruizzs@*/nestjs-task-management@*:*"
+            ]
           }
         }
       }
